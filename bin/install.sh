b6c0cc8aeee194098bbef79e520bc99cc86d8ec3
Mitsuyuki Shiiba <bufferings+github@gmail.com>
2021-11-04 01:51:47 +0900

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+set -eu
 
+dotdir=$(cd $(dirname $0)/..; pwd)
+
+for f in $dotdir/.??*; do
+  [ "$f" = ".git" ] && continue
+  [ "$f" = ".DS_Store" ] && continue
+
+  ln -snfv "$f" "$HOME"/"$f"
+done
